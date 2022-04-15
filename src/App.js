4ddcb87143f3a5a32549471b59141a3a7c6768bd
Steve Boyer <stephen.a.boyer@gmail.com>
2022-04-15 14:56:40 -0500

    Make corrections in accordance with react-bootstrap syntax

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button variant="secondary">
-          <img src="images/metamask.svg" alt="MetaMask" width="50" height="50" />
-        </button>
+        <Button variant="secondary">
+          <img src="images/metamask.svg" alt="MetaMask" width="50" height="50" /> Connect to MetaMask
+        </Button>
         <div className="mt-2 mb-2">
           Connected Account:
         </div>
-        <button variant="danger">
+        <Button variant="danger">
           Disconnect MetaMask <img src="images/noun_waving_3666509.svg" width="50" height="50" />
-        </button>
+        </Button>
       </header>
     </div>
   );
