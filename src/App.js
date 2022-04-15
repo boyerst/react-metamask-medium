51dd43050b2af1602594b862c7a8c45537df46df
Steve Boyer <stephen.a.boyer@gmail.com>
2022-04-15 14:40:27 -0500

    Add buttons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <button variant="secondary">
+          <img src="images/metamask.svg" alt="MetaMask" width="50" height="50" />
+        </button>
+        <div className="mt-2 mb-2">
+          Connected Account:
+        </div>
+        <button variant="danger">
+          Disconnect MetaMask <img src="images/noun_waving_3666509.svg" width="50" height="50" />
+        </button>
       </header>
     </div>
   );
